1c40f0a46b49230f808fd8ed37d2e305a4d8f357
ext4 <ghaleb.sarsour2007@gmail.com>
2024-12-30 21:30:42 -0500

    Battery Optimizations

--- configs/battery.nix
+++ configs/battery.nix
@@ -1,16 +1,32 @@
 { config, pkgs, ... }:
 
 {
-    services.power-profiles-daemon.enable = false;
-    services.tlp ={
-        enable = true;
-        settings = {
-            CPU_BOOST_ON_AC = 1;
-            CPU_BOOST_ON_BAT = 0;
-            CPU_SCALING_GOVERNOR_ON_AC = "performance";
-            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-            CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    powerManagement = {
+      enable = true;
+      powertop.enable = true;
+      cpuFreqGovernor = "powersave";
+    };
+
+    services = { 
+        power-profiles-daemon.enable = false;  
+        thermald.enable = true; 
+        system76-scheduler = {
+            enable = true;
+            useStockConfig = true;
+            settings.cfsProfiles.enable = true;
+        };
+        tlp = {
+            enable = true;
+            settings = {
+                CPU_BOOST_ON_AC = 1;
+                CPU_BOOST_ON_BAT = 0;
+                CPU_SCALING_GOVERNOR_ON_AC = "performance";
+                CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+                CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+                CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+                START_CHARGE_THRESH_BAT0 = 81;
+                STOP_CHARGE_THRESH_BAT0 = 86;
+            };
         };
     };
 }
