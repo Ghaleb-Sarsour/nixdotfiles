ad9a2bed77616fea8cff44e13b15dbce3cbde878
ext4 <ghaleb.sarsour2007@gmail.com>
2025-01-20 17:36:05 -0500

    Added tables to neorg

--- hmconfigs/neovim/nconfig/lua/axt4/plugins/init.lua
+++ hmconfigs/neovim/nconfig/lua/axt4/plugins/init.lua
@@ -1,12 +1,12 @@
 return {
     "nvim-lua/plenary.nvim",
     "VonHeikemen/lsp-zero.nvim",
---    "neovim/nvim-lspconfig",
     "hrsh7th/cmp-nvim-lsp",
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "windwp/nvim-ts-autotag",
     "brianhuster/live-preview.nvim",
     "itchyny/calendar.vim",
+    "dhruvasagar/vim-table-mode",
 }
 
