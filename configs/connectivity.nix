69ec7788cdc39de10a8c144e27ba567422c2855b
ext4 <ghaleb.sarsour2007@gmail.com>
2025-03-12 12:05:37 -0400

    closed unneeded ports

--- configs/connectivity.nix
+++ configs/connectivity.nix
@@ -20,9 +20,11 @@
   #Firewall
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 80 443 22 53 21 20 8006 3389 ];
+    # allowedTCPPorts = [ 80 443 22 53 21 20 8006 3389 ];
+    allowedTCPPorts = [22 443];
     allowedTCPPortRanges = [];
-    allowedUDPPorts = [ 3389 ];
+    # allowedUDPPorts = [ 3389 ];
+    allowedUDPPorts = [ ];
     allowedUDPPortRanges = [];
   };
 
