b59053a63dd095c11bd05042ebed37cec6fdd13e
ext4 <ghaleb.sarsour2007@gmail.com>
2025-03-21 09:37:42 -0400

    wezterm setup

---
+++ hmconfigs/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Importing wezterm api
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- Options:
+
+config.color_scheme = "Moonfly (Gogh)" 
+
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 20
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.9
+
+
+-- Return config
+return config
---
+++ hmconfigs/wezterm/wezterm.nix
@@ -0,0 +1,5 @@
+{ config, pkgs, ... }:
+
+{
+    home.file.".wezterm.lua".source = ./wezterm.lua;
+}
