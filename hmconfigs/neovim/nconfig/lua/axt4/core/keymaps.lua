5c99d9a3096dc6d4357c92eb460e6955f601b6fa
ext4 <ghaleb.sarsour2007@gmail.com>
2025-03-25 15:20:05 -0400

    Changed terminal keymaps in nvim

--- hmconfigs/neovim/nconfig/lua/axt4/core/keymaps.lua
+++ hmconfigs/neovim/nconfig/lua/axt4/core/keymaps.lua
@@ -39,8 +39,8 @@ keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false,
 --keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
 
 -- Toggle Term:
-keymap.set('n', '<leader>lt', ':ToggleTerm direction=float name=', {noremap = true, silent = false; desc = "Open new floating terminal"})
-keymap.set('n', '<leader>ll', ':TermSelect<CR>', {noremap = true, silent = false, desc = "Select open terminal"})
+keymap.set('n', '<leader>rt', ':ToggleTerm direction=float name=', {noremap = true, silent = false; desc = "Open new floating terminal"})
+keymap.set('n', '<leader>rl', ':TermSelect<CR>', {noremap = true, silent = false, desc = "Select open terminal"})
 
 --Neovide Copy/Paste
 if vim.g.neovide then
