ca9348cb2429547bf3d7072113ac3a7c93bb0feb
ext4 <ghaleb.sarsour2007@gmail.com>
2025-03-31 08:43:34 -0400

    Updated nixos aliases with nh command and added nix flake update to flupdate

--- hmconfigs/shell/sh.nix
+++ hmconfigs/shell/sh.nix
@@ -4,8 +4,10 @@ let
   aliases = {
     ll = "ls -l";
     ".." = "cd ..";
-    "hmapply" = "home-manager switch --flake .";
-    "flapply" = "sudo nixos-rebuild switch --flake .";
+    # "hmapply" = "home-manager switch --flake .";
+    # "flapply" = "sudo nixos-rebuild switch --flake .";
+    "hmapply" = "nh home switch ~/.dotfiles/";
+    "flapply" = "nix flake update && nh os switch ~/.dotfiles/";
     "flupdate" = "nh os switch --update ~/.dotfiles/";
     zjs = "steam-run ~/zulu11.76.21-ca-fx-jdk11.0.25-linux_x64/bin/java";
     zjcs = "steam-run ~/zulu11.76.21-ca-fx-jdk11.0.25-linux_x64/bin/javac";
