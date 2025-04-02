192a5a54a4ef4e05b642ba93ed6463ddb84e5c1a
ext4 <ghaleb.sarsour2007@gmail.com>
2025-04-02 11:01:29 -0400

    edits

--- hmconfigs/neovim/nconfig/lua/axt4/plugins/snacks.lua
+++ hmconfigs/neovim/nconfig/lua/axt4/plugins/snacks.lua
@@ -31,7 +31,6 @@ return {
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = '~/.dotfiles/hmconfigs/neovim/nconfig/lua/axt4/plugins/'})" },
             { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
           -- Used by the `header` section
           header = [[
@@ -70,6 +69,7 @@ return {
         sections = {
           { pane = 1, section = "header" },
           { pane = 1, section = "keys", gap = 1, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           { section = "startup" },
         },
     },
